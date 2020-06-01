25bb5a1551262f680b6fec41eebcc7bc8234560b
mariobot <mariobot113@hotmail.com>
2020-05-31 20:59:19 -0500

    Azure functions lab

---
+++ azure-functions/index.js
@@ -0,0 +1,30 @@
+module.exports = function (context, req) {
+    context.log('Drive Gear Temperature Service triggered');
+    if (req.body && req.body.readings) {
+        req.body.readings.forEach(function(reading) {
+
+            if(reading.temperature<=25) {
+                reading.status = 'OK';
+            } else if (reading.temperature<=50) {
+                reading.status = 'CAUTION';
+            } else {
+                reading.status = 'DANGER'
+            }
+            context.log('Reading is ' + reading.status);
+        });
+
+        context.res = {
+            // status: 200, /* Defaults to 200 */
+            body: {
+                "readings": req.body.readings
+            }
+        };
+    }
+    else {
+        context.res = {
+            status: 400,
+            body: "Please send an array of readings in the request body"
+        };
+    }
+    context.done();
+};
