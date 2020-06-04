8f344d420851288c12165564d444894238657193
Mario Botero <mariobot113@hotmail.com>
2020-06-04 10:34:43 -0500

    Update project

--- web-api-core/Contoso.Api/Controllers/ProductsController.cs
+++ web-api-core/Contoso.Api/Controllers/ProductsController.cs
@@ -24,11 +24,59 @@ namespace Contoso.Api.Controllers
             _context.Products.ToList();
 
         // GET by ID action
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetById(long id)
+        {
+            var product = await _context.Products.FindAsync(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return product;
+        }
 
         // POST action
+        [HttpPost]
+        public async Task<IActionResult> Create(Product product)
+        {
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetById), new { id = product.Id }, product);
+        }
 
         // PUT action
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(long id, Product product)
+        {
+            if (id != product.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(product).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
 
         // DELETE action
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(long id)
+        {
+            var product = await _context.Products.FindAsync(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
     }
 }
