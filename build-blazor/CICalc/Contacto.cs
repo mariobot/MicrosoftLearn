5829cf1e36a4fee00f906c9be868e50395c7770f
mariobot <mariobot113@hotmail.com>
2020-06-10 16:41:54 -0500

    Contact at blazor

---
+++ build-blazor/CICalc/Contacto.cs
@@ -0,0 +1,13 @@
+namespace CICalc
+{
+    public class Contacto
+    {
+        public string Nombre { get; set; }
+
+        public string Apellido { get; set; }
+
+        public string Celular { get; set; }
+
+        public string Direccion { get; set; }
+    }
+}
