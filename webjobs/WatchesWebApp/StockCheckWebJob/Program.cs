0edc25fc235208c1756e9c82d2fc2499f7eeb0b6
mariobot <mariobot113@hotmail.com>
2020-06-16 17:31:26 -0500

    Add functionality to the webjobs

--- webjobs/WatchesWebApp/StockCheckWebJob/Program.cs
+++ webjobs/WatchesWebApp/StockCheckWebJob/Program.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
+using System.Configuration;
+using System.Threading;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Queue;
 
 namespace StockCheckWebJob
 {
@@ -24,6 +28,22 @@ namespace StockCheckWebJob
             var host = new JobHost(config);
             // The following code ensures that the WebJob will be running continuously
             host.RunAndBlock();
+
+            var queue = CloudStorageAccount.Parse(ConfigurationManager.ConnectionStrings["StorageAccount"].ConnectionString)
+                            .CreateCloudQueueClient()
+                            .GetQueueReference("stockchecks");
+
+            queue.CreateIfNotExists();
+
+            while (true)
+            {
+                var timestamp = DateTimeOffset.UtcNow.ToString("s");
+
+                var message = new CloudQueueMessage($"Stock check at {timestamp} completed");
+                queue.AddMessage(message);
+
+                Thread.Sleep(TimeSpan.FromSeconds(30));
+            }
         }
     }
 }
