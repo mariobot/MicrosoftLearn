6f7bc78b83002c786e546ad70619c1e2fe398c97
mariobot <mariobot113@hotmail.com>
2020-10-16 16:27:05 -0500

    Add Core and UseCases projects

---
+++ BlazorCommerce.CoreBusiness/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorCommerce.CoreBusiness.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Brand { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string ImageLink { get; set; }
+
+    }
+}
--- BlazorCommerce/Startup.cs
+++ BlazorCommerce/Startup.cs
@@ -45,7 +45,7 @@ namespace BlazorCommerce
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
