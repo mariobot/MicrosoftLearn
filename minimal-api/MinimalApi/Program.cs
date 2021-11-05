3ce6b924142f1569df1c35565a58a5a96821e380
mariobot <mariobot113@hotmail.com>
2021-11-05 15:15:53 -0500

    Add FontMinimalApi project

--- minimal-api/MinimalApi/Program.cs
+++ minimal-api/MinimalApi/Program.cs
@@ -1,9 +1,15 @@
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddCors();
 builder.Services.AddDbContext<TodoDb>(opt => opt.UseInMemoryDatabase("TodoList"));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 var app = builder.Build();
+
+app.UseCors(builder => builder
+ .AllowAnyOrigin()
+ .AllowAnyMethod()
+ .AllowAnyHeader());
 
 app.MapGet("/", () => "Hello World!");
 
