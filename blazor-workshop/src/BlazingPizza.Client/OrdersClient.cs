c5ab570e93b2e8c8c408c5a0c3c401f9cf0c5703
mariobot <mariobot113@hotmail.com>
2021-11-26 16:19:52 -0500

    Progressive Web App (PWA) features

--- blazor-workshop/src/BlazingPizza.Client/OrdersClient.cs
+++ blazor-workshop/src/BlazingPizza.Client/OrdersClient.cs
@@ -31,5 +31,12 @@ namespace BlazingPizza.Client
             var orderId = await response.Content.ReadFromJsonAsync<int>();
             return orderId;
         }
+
+        public async Task SubscribeToNotifications(NotificationSubscription subscription)
+        {
+            var response = await httpClient.PutAsJsonAsync("notifications/subscribe", subscription);
+            response.EnsureSuccessStatusCode();
+        }
+
     }
 }
