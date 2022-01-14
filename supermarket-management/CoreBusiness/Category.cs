37d8606dc075a2d2c0fe636db09e8dd3e5891ffd
mariobot <mariobot113@hotmail.com>
2022-01-13 22:35:56 -0500

    View Categories

---
+++ supermarket-management/CoreBusiness/Category.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CoreBusiness
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ supermarket-management/Plugins.DataStore.InMemory/CategoryRepository.cs
@@ -0,0 +1,31 @@
+﻿using CoreBusiness;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UseCases.DataStorePluginInterfaces;
+
+namespace Plugins.DataStore.InMemory
+{
+    public class CategoryInMemoryRepository : ICategoryRepository
+    {
+        private List<Category> categories;
+
+        public CategoryInMemoryRepository()
+        {
+            categories = new List<Category>()
+            {
+                new Category { CategoryId = 1, Name = "Bebidas", Description = "Categoria de Bebidas"},
+                new Category { CategoryId = 1, Name = "Panaderia", Description = "Categoria de Panaderia"},
+                new Category { CategoryId = 1, Name = "Carnes", Description = "Categoria de Carnes"}
+            };
+        
+        }
+
+        public IEnumerable<Category> GetCategories()
+        {
+            return categories;        
+        }
+    }
+}
