2e37ea5658f40a39027688b9f388d1a0daa3bb0b
mariobot <mariobot113@hotmail.com>
2022-02-04 16:57:09 -0500

    Read cookie with authentication middleware

--- identity-asp-core/WebApp_Security/WebApp/Startup.cs
+++ identity-asp-core/WebApp_Security/WebApp/Startup.cs
@@ -23,7 +23,7 @@ namespace WebApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddAuthentication().AddCookie("MyCookieAuth", options =>
+            services.AddAuthentication("MyCookieAuth").AddCookie("MyCookieAuth", options =>
             {
                 options.Cookie.Name = "MyCookieAuth";
             });
@@ -52,6 +52,7 @@ namespace WebApp
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
