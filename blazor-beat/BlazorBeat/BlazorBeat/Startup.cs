9b89f1fce5fa1f3e638f2e37be3169237d257345
mariobot <mariobot113@hotmail.com>
2022-02-17 12:02:03 -0500

    Remove some files

--- blazor-beat/BlazorBeat/BlazorBeat/Startup.cs
+++ blazor-beat/BlazorBeat/BlazorBeat/Startup.cs
@@ -1,4 +1,3 @@
-using BlazorBeat.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
@@ -28,7 +27,7 @@ namespace BlazorBeat
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
+            //services.AddSingleton<WeatherForecastService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
