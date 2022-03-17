5156619469d17c869c1d2b4b7ac4acf70afe2399
mariobot <mariobot113@hotmail.com>
2022-03-17 16:19:20 -0500

    Add Update functionality

---
+++ blazor-material-ui/BlazorMaterialUi/Pages/UpdateProduct.razor.cs
@@ -0,0 +1,61 @@
+﻿using BlazorMaterialUi.HttpRepository;
+using BlazorMaterialUi.Shared;
+using Entities.Models;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using System;
+using System.Threading.Tasks;
+
+namespace BlazorMaterialUi.Pages
+{
+    public partial class UpdateProduct
+    {
+        [Parameter]
+        public string productId { get; set; }
+
+        private Product _product = new Product();
+        private DateTime? _date = DateTime.Today;
+        
+        [Inject]
+        public IHttpClientRepository Repository { get; set; }
+        [Inject]
+        public IDialogService Dialog { get; set; }
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            Guid id;
+            Guid.TryParse(productId, out id);
+            _product = await Repository.GetProduct(id);
+        }
+
+        private async Task Update()
+        {
+            _product.ManufactureDate = (DateTime)_date;
+            await Repository.UpdateProduct(_product);
+            await ExecuteDialogAsync();
+        }
+
+        private async Task ExecuteDialogAsync()
+        {
+            var parameters = new DialogParameters
+            {
+                { "Content", "You have succesfully update the product."},
+                { "ButtonColor", Color.Primary },
+                { "ButtonText", "OK" }
+            };
+
+            var dialog = Dialog.Show<DialogNotification>("Product updated", parameters);
+            var result = await dialog.Result;
+            if (!result.Cancelled)
+            {
+                bool.TryParse(result.Data.ToString(), out bool shouldNavigate);
+                if (shouldNavigate)
+                    NavigationManager.NavigateTo("/fetchdata");
+            }
+        }
+
+        public void SetImgUrl(string imgUrl) => _product.ImageUrl = imgUrl;
+    }
+}
