9630f1d20dd1cf6f6891f632b2c2e131e5f69011
mariobot <mariobot113@hotmail.com>
2022-04-06 15:49:08 -0500

    Update location

---
+++ angular-js/http/controller.js
@@ -0,0 +1,26 @@
+var app = angular.module("MyFirstApp",[]).
+controller("FirstController", function($scope,$http){
+    $scope.posts = [];
+    $scope.newPost = {};
+    $http.get("http://jsonplaceholder.typicode.com/posts")
+    .then(function (response){
+        console.log(response);
+        $scope.posts = response.data;
+    },function (error){
+        console.log(error);
+    });
+
+    $scope.addPost = function(){
+        $http.post("http://jsonplaceholder.typicode.com/posts",{
+            title: $scope.newPost.title,
+            body: $scope.newPost.body,
+            userId: 1
+        })
+        .then(function (data,status,headers,){
+            console.log(response);
+            $scope.posts = response.data;
+        },function (error){
+            console.log(error);
+        });
+    }
+});
