0b9c9e5e1b7d1c8a66cdbce1fa298a1155a23f60
mariobot <mariobot113@hotmail.com>
2022-04-08 11:44:21 -0500

    Add custom directive

--- angular-js/src/controller.js
+++ angular-js/src/controller.js
@@ -1,5 +1,16 @@
-var app = angular.module("CustomDirective",[]).
-controller("AppController", function($scope,$http){
+var app = angular.module("CustomDirective",[])
+.directive("backImg",function(){
+    return function(scope, element, attrs){
+        attrs.$observe("backImg",function(value){
+            element.css({
+                "background": "url("+value+")",
+                "background-size": "cover",
+                "background-position": "center"
+            });
+        });
+    }
+})
+.controller("AppController", function($scope,$http){
     $http.get("https://api.github.com/users/mariobot/repos")
     .then(function(data){
         $scope.repos = data.data;
