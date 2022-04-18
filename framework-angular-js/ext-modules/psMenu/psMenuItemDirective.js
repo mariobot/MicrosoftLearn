bb449e1a4f4e143902fde9b891f0c21a90abd834
mariobot <mariobot113@hotmail.com>
2022-04-18 09:29:46 -0500

    Showing the Menu

--- framework-angular-js/ext-modules/psMenu/psMenuItemDirective.js
+++ framework-angular-js/ext-modules/psMenu/psMenuItemDirective.js
@@ -3,6 +3,9 @@
 angular.module("psMenu").directive("psMenuItem", function(){
     return {
         require: "^psMenu",
+        scope: {
+            label: '@'
+        },
         templateUrl: "ext-modules/psMenu/psMenuItemTemplate.html",
         link: function(scope, el, attr, ctrl){
 
