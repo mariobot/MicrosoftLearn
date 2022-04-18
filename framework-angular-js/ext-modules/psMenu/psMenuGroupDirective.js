577f15ef9776b0427e023f873d589bdf71eff9fa
mariobot <mariobot113@hotmail.com>
2022-04-18 15:20:14 -0500

    Grouping Menu Items, A custom Directive for Groups

---
+++ framework-angular-js/ext-modules/psMenu/psMenuGroupDirective.js
@@ -0,0 +1,16 @@
+'use strict'
+
+angular.module("psMenu").directive("psMenuGroup", function(){
+    return {
+        require: '^psMenu',
+        transclude: true,
+        scope:{
+            label: '@',
+            icon: '@'
+        },
+        templateUrl : "ext-modules/psMenu/psMenuGroupTemplate.html",
+        link: function(scope, el, attrs, ctrl){
+            
+        }
+    }
+});
