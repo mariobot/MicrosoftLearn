f13a30d29be254cec53aac2a6a501be5ad273447
mariobot <mariobot113@hotmail.com>
2022-04-20 15:37:16 -0500

    Creating Widget Custom

---
+++ framework-angular-js/app/widgets/wwaEmployee/wwaEmployeeDirective.js
@@ -0,0 +1,12 @@
+﻿"use strict";
+
+angular.module('app').directive('wwaEmployee',
+    ['dataService',
+    function (dataService) {
+        return {
+            templateUrl: 'app/widgets/wwaEmployee/wwaEmployeeTemplate.html',
+            link: function (scope, el, attrs) {
+
+            }
+        };
+    }]);
---
+++ framework-angular-js/app/widgets/wwaInventory/wwaInventoryDirective.js
@@ -0,0 +1,12 @@
+﻿"use strict";
+
+angular.module('app').directive('wwaInventory',
+    ['dataService',
+    function (dataService) {
+        return {
+            templateUrl: 'app/widgets/wwaInventory/wwaInventoryTemplate.html',
+            link: function (scope, el, attrs) {
+
+            }
+        };
+    }]);
