ec5da437b8064993e93096b147f52b37b12fe23e
mariobot <mariobot113@hotmail.com>
2022-04-25 11:20:38 -0500

    A button to add widget, adding widget to dashboard

--- framework-angular-js/app/directives/wwaDashboardDirective.js
+++ framework-angular-js/app/directives/wwaDashboardDirective.js
@@ -19,6 +19,46 @@ angular.module('app').directive("wwaDashboard",[function(){
                 swapping: false
             };
 
+            scope.widgetDefinitions = [
+                {
+                    title: 'Temperature',
+                    settings:{
+                        sizeX: 3,
+                        sizeY: 3,
+                        minSizeX: 2,
+                        minSizeY: 2,                        
+                        template: "<wwa-temperature></wwa-temperature>",
+                        widgetSettings:{
+                            id: 1000
+                        }
+                    }
+                },
+                {
+                    title: 'Employee',
+                    settings:{
+                        sizeX: 5,
+                        sizeY: 3,
+                        template: "<wwa-employee></wwa-employee>",
+                        widgetSettings:{
+                            id: 5001
+                        }
+                    }
+                },
+                {
+                    title: "Inventory",
+                    settings:{
+                        sizeX: 5,
+                        sizeY: 3,
+                        row: 3,
+                        col: 5,
+                        template: "<wwa-inventory></wwa-inventory>",
+                        widgetSettings:{
+                            id: 1002
+                        }
+                    }
+                }
+            ]
+
             scope.widgets = [
                 {
                     title: "First",
