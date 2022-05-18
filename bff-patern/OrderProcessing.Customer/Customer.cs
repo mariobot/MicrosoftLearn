b543b4df2394269e27898c2ddc848f0722c86f61
mariobot <mariobot113@hotmail.com>
2022-05-18 10:14:57 -0500

    Remove default files

---
+++ bff-patern/OrderProcessing.Customer/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace OrderProcessing.Customer
+{
+    public class Customer
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string EmailAddress { get; set; }
+    }
+}
