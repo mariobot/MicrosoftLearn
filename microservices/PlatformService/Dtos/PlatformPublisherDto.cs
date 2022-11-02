5402211931f11c599da3303b760364a54f13228d
mariobot <mariobot113@hotmail.com>
2022-11-02 16:53:55 -0500

    Create rabbit kubernete pod

---
+++ microservices/PlatformService/Dtos/PlatformPublisherDto.cs
@@ -0,0 +1,11 @@
+namespace PlatformService.Dtos
+{
+    public class PlatformPublisherDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Event { get; set; }
+    }
+}
