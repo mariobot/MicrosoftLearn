e28e43868940e60e72b5ed0f0f22dfbdc6399a13
mariobot <mariobot113@hotmail.com>
2022-11-25 15:43:33 -0500

    Add API Project and reuse mediator

---
+++ mediator-pattern/DemoApi/Controllers/PersonController.cs
@@ -0,0 +1,45 @@
+﻿using DemoLibrary.Models;
+using DemoLibrary.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DemoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        private readonly IMediator mediator;
+
+        public PersonController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        // GET: api/<PersonController>
+        [HttpGet]
+        public async Task<List<PersonModel>> GetPersons()
+        {
+            return await mediator.Send(new GetPersonListQuery());
+        }
+
+        // GET api/<PersonController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<PersonController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+    }
+}
