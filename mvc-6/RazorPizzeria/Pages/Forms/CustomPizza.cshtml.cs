302a2e0f9653f6fc49ca776b82d4ab3be3cea588
mariobot <mariobot113@hotmail.com>
2022-11-29 17:14:08 -0500

    Set up data model

--- mvc-6/RazorPizzeria/Pages/Forms/CustomPizza.cshtml.cs
+++ mvc-6/RazorPizzeria/Pages/Forms/CustomPizza.cshtml.cs
@@ -1,10 +1,13 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPizzeria.Models;
 
 namespace RazorPizzeria.Pages.Forms
 {
     public class CustomPizzaModel : PageModel
     {
+        [BindProperty]
+        public PizzasModel Pizza { get; set; }
         public void OnGet()
         {
         }
