097f8e0d2a4028cf149b39f8df20d60cce9a755a
mariobot <mariobot113@hotmail.com>
2022-11-30 14:52:36 -0500

    IActionResult OnPost action

--- mvc-6/RazorPizzeria/Models/PizzasModel.cs
+++ mvc-6/RazorPizzeria/Models/PizzasModel.cs
@@ -4,7 +4,7 @@
     {
         public string ImageTitle { get; set; }
         public string PizzaName { get; set; }
-        public float BasePrice { get; set; }
+        public float BasePrice { get; set; } = 2;
         public bool TomatoSauce { get; set; }
         public bool Cheese { get; set; }
         public bool Peperoni { get; set; }
--- mvc-6/RazorPizzeria/Pages/Forms/CustomPizza.cshtml.cs
+++ mvc-6/RazorPizzeria/Pages/Forms/CustomPizza.cshtml.cs
@@ -8,8 +8,25 @@ namespace RazorPizzeria.Pages.Forms
     {
         [BindProperty]
         public PizzasModel Pizza { get; set; }
+        public float PizzaPrice { get; set; }
         public void OnGet()
         {
+        }
+
+        public IActionResult OnPost()
+        {
+            PizzaPrice = Pizza.BasePrice;
+
+            if (Pizza.TomatoSauce) PizzaPrice += 1;
+            if (Pizza.Cheese) PizzaPrice += 1;
+            if (Pizza.Peperoni) PizzaPrice += 1;
+            if (Pizza.Mushroom) PizzaPrice += 1;
+            if (Pizza.Tuna) PizzaPrice += 1;
+            if (Pizza.Pineapple) PizzaPrice += 10;
+            if (Pizza.Ham) PizzaPrice += 1;
+            if (Pizza.Beef) PizzaPrice += 1;
+
+            return RedirectToPage("/Checkout/Checkout", new { Pizza.PizzaName, PizzaPrice });
         }
     }
 }
