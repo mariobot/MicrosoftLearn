398044b1fb1f341cd635348eff535c8ca0fd013d
mariobot <mariobot113@hotmail.com>
2022-11-30 16:37:45 -0500

    Final chapter add orders

---
+++ mvc-6/RazorPizzeria/Pages/Orders.cshtml.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPizzeria.Data;
+using RazorPizzeria.Models;
+
+namespace RazorPizzeria.Pages
+{
+    public class OrdersModel : PageModel
+    {
+        public List<PizzaOrder> PizzaOrders = new List<PizzaOrder>();
+
+        public readonly ApplicationDbContext _context;
+        public OrdersModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public void OnGet()
+        {
+            PizzaOrders = _context.PizzaOrders.ToList();
+        }
+    }
+}
