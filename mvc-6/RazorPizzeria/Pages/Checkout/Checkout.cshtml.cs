e403dc9548798db4b3a131cc66ec2f2f4cc0700e
mariobot <mariobot113@hotmail.com>
2022-11-30 15:14:32 -0500

    OnGet and display data

--- mvc-6/RazorPizzeria/Pages/Checkout/Checkout.cshtml.cs
+++ mvc-6/RazorPizzeria/Pages/Checkout/Checkout.cshtml.cs
@@ -3,10 +3,24 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace RazorPizzeria.Pages.Checkout
 {
+    [BindProperties(SupportsGet =true)]
     public class CheckoutModel : PageModel
-    {
+    {   
+        public string PizzaName { get; set; }
+
+        public float PizzaPrice { get; set; }
+
+        public string ImageTitle { get; set; }
         public void OnGet()
         {
+            if (string.IsNullOrWhiteSpace(PizzaName))
+            {
+                PizzaName = "Custom";
+            }
+            if (string.IsNullOrWhiteSpace(ImageTitle))
+            {
+                ImageTitle = "Create";
+            }
         }
     }
 }
