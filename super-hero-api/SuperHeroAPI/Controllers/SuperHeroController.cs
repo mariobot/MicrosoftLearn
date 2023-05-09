67fe31d38778683339cf0e19ff2839a160c29f99
mariobot <mariobot113@hotmail.com>
2023-05-09 16:42:17 -0500

    Add Controller and Model.

---
+++ super-hero-api/SuperHeroAPI/Controllers/SuperHeroController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SuperHeroAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> Get()
+        {
+            var heroes = new List<SuperHero>
+            { new SuperHero {
+                Id = 1,
+                Name = "Spider Man",
+                FirstName = "Peter",
+                LastName = "Parker",
+                Place = "New York City"}
+            };
+
+            return Ok(heroes);
+        
+        }
+    }
+}
--- super-hero-api/SuperHeroAPI/SuperHero.cs
+++ super-hero-api/SuperHeroAPI/SuperHero.cs
@@ -6,6 +6,6 @@
         public string Name { get; set; } = string.Empty;
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
-        public string MyProperty { get; set; } = string.Empty;
+        public string Place { get; set; } = string.Empty;
     }
 }
