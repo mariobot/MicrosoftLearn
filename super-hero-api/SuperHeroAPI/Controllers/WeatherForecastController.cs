80b28f9715d5009e3fb829ce7ea301ed2b3f22cb
mariobot <mariobot113@hotmail.com>
2023-05-09 16:33:46 -0500

    Create Model

--- super-hero-api/SuperHeroAPI/Controllers/WeatherForecastController.cs
+++ super-hero-api/SuperHeroAPI/Controllers/WeatherForecastController.cs
@@ -8,8 +8,8 @@ namespace SuperHeroAPI.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
 
         private readonly ILogger<WeatherForecastController> _logger;
 
@@ -18,7 +18,7 @@ namespace SuperHeroAPI.Controllers
             _logger = logger;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
+        [HttpGet(Name = "weather")]
         public IEnumerable<WeatherForecast> Get()
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
