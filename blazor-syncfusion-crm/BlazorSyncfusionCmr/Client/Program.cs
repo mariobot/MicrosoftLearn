13c9e6363ae5bfb0d4ebabeb88b03ecbbc42e52e
mariobot <mariobot113@hotmail.com>
2023-05-15 16:07:12 -0500

    Add Syncfusion to project

---
+++ blazor-syncfusion-crm/BlazorSyncfusionCmr/Client/Program.cs
@@ -0,0 +1,17 @@
+using BlazorSyncfusionCmr.Client;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Syncfusion.Blazor;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddHttpClient("BlazorSyncfusionCmr.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
+
+// Supply HttpClient instances that include access tokens when making requests to the server project
+builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("BlazorSyncfusionCmr.ServerAPI"));
+
+builder.Services.AddSyncfusionBlazor();
+
+await builder.Build().RunAsync();
