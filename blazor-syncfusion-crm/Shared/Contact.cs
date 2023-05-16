3439e29d081d5145acfd244e6c0788d53bf7682c
mariobot <mariobot113@hotmail.com>
2023-05-16 16:42:30 -0500

    Add syncfusion DataGrid function

--- blazor-syncfusion-crm/Shared/Contact.cs
+++ blazor-syncfusion-crm/Shared/Contact.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Text.Json.Serialization;
-using System.Threading.Tasks;
+﻿using System.Text.Json.Serialization;
 
 namespace BlazorSyncfusionCmr.Shared
 {
@@ -14,10 +9,11 @@ namespace BlazorSyncfusionCmr.Shared
         public string LastName { get; set; } = string.Empty;
         public string NickName { get; set; } = string.Empty;
         public string Place { get; set; } = string.Empty;
-
+        public bool IsDeleted { get; set; }
         public DateTime? DateOfBirth { get; set; }
-        public DateTime? DateCreated { get; set; } = DateTime.Now;
-
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+        public DateTime? DateUpdated { get; set; }
+        public DateTime? DateDeleted { get; set; }
         [JsonIgnore]
         public List<Note> Notes { get; set; } = new List<Note>();
     }
--- blazor-syncfusion-crm/Shared/Note.cs
+++ blazor-syncfusion-crm/Shared/Note.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BlazorSyncfusionCmr.Shared
+﻿namespace BlazorSyncfusionCmr.Shared
 {
     public class Note
     {
+        public int Id { get; set; }
+        public required string Text { get; set; }
+        public int? ContactId { get; set; }
+        public Contact? Contact { get; set; }
+        public DateTime DateCreated { get; set;} = DateTime.Now;
     }
 }
