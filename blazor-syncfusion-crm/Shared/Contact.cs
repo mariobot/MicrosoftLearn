72bb0c22aac344478f86baa64b2ec3257fd94243
mariobot <mariobot113@hotmail.com>
2023-05-16 16:06:55 -0500

    Create new models

---
+++ blazor-syncfusion-crm/Shared/Contact.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace BlazorSyncfusionCmr.Shared
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string NickName { get; set; } = string.Empty;
+        public string Place { get; set; } = string.Empty;
+
+        public DateTime? DateOfBirth { get; set; }
+        public DateTime? DateCreated { get; set; } = DateTime.Now;
+
+        [JsonIgnore]
+        public List<Note> Notes { get; set; } = new List<Note>();
+    }
+}
