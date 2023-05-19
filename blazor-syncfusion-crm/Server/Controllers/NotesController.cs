248fa5d5b0dbd9db794327d614d5a1fb000cb1a4
mariobot <mariobot113@hotmail.com>
2023-05-19 16:18:54 -0500

    Add get by id action

--- blazor-syncfusion-crm/Server/Controllers/NotesController.cs
+++ blazor-syncfusion-crm/Server/Controllers/NotesController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using BlazorSyncfusionCmr.Server.Data;
+using BlazorSyncfusionCmr.Shared;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BlazorSyncfusionCmr.Server.Controllers
 {
@@ -7,5 +10,29 @@ namespace BlazorSyncfusionCmr.Server.Controllers
     [ApiController]
     public class NotesController : ControllerBase
     {
+        private readonly DataContext dataContext;
+
+        public NotesController(DataContext dataContext)
+        {
+            this.dataContext = dataContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Note>>> GetAllNotes()
+        {
+            return await dataContext.Notes
+                .Include(n => n.Contact)
+                .OrderByDescending(n => n.DateCreated)
+                .ToListAsync();
+        }
+
+        [HttpGet("{contactId}")]
+        public async Task<ActionResult<List<Note>>> GetNotesFromContact(int contactId)
+        {
+            return await dataContext.Notes
+                .Where(x => x.ContactId == contactId)
+                .OrderByDescending(n => n.DateCreated)
+                .ToListAsync();
+        }
     }
 }
