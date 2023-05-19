1ec0b6a9dfb31a373e4c5a6a6628451ffae67820
mariobot <mariobot113@hotmail.com>
2023-05-19 16:30:05 -0500

    Create and Delete note controller actions.

--- blazor-syncfusion-crm/Server/Controllers/NotesController.cs
+++ blazor-syncfusion-crm/Server/Controllers/NotesController.cs
@@ -1,8 +1,10 @@
-﻿using BlazorSyncfusionCmr.Server.Data;
+﻿using BlazorSyncfusionCmr.Client.Pages;
+using BlazorSyncfusionCmr.Server.Data;
 using BlazorSyncfusionCmr.Shared;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Syncfusion.Blazor.Diagram;
 
 namespace BlazorSyncfusionCmr.Server.Controllers
 {
@@ -31,6 +33,36 @@ namespace BlazorSyncfusionCmr.Server.Controllers
         {
             return await dataContext.Notes
                 .Where(x => x.ContactId == contactId)
+                .OrderByDescending(n => n.DateCreated)
+                .ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<Note>>> CreateNotes(Note note)
+        {
+            dataContext.Notes.Add(note);
+            await dataContext.SaveChangesAsync();
+
+            return await dataContext.Notes
+                .Where(x => x.ContactId == note.ContactId)
+                .OrderByDescending(n => n.DateCreated)
+                .ToListAsync();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<Note>>> DeleteNote(int id)
+        {
+            var dbNote = await dataContext.Notes.FindAsync(id);
+            if (dbNote is null) 
+            {
+                return BadRequest("Note not found.");
+            }
+
+            this.dataContext.Notes.Remove(dbNote);
+            await dataContext.SaveChangesAsync();
+
+            return await dataContext.Notes
+                .Where(x => x.ContactId == dbNote.ContactId)
                 .OrderByDescending(n => n.DateCreated)
                 .ToListAsync();
         }
