526f7f2caa7389d2017fb775e4951f13b681341d
mariobot <mariobot113@hotmail.com>
2023-05-19 16:16:55 -0500

    Add Notes controller and get action

---
+++ blazor-syncfusion-crm/Server/Controllers/NotesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorSyncfusionCmr.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotesController : ControllerBase
+    {
+    }
+}
