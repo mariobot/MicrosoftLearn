1328fc16c48f2fea2e8822daa636a2a8afbce261
mariobot <mariobot113@hotmail.com>
2023-06-22 18:45:32 -0500

    Add DataContext class.

---
+++ varify-email/Data/DataContext.cs
@@ -0,0 +1,22 @@
+﻿
+
+namespace VarifyEmail.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder
+                .UseSqlServer("Data Source=MBOTERO\\MBOTERO;Integrated Security=True");
+        }
+
+        public DbSet<User> Users => Set<User>();
+    }
+}
--- varify-email/Program.cs
+++ varify-email/Program.cs
@@ -1,3 +1,7 @@
+global using Microsoft.EntityFrameworkCore;
+global using VarifyEmail.Model;
+using VarifyEmail.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DataContext>();
 
 var app = builder.Build();
 
