2e208353e6224c7001a49cc6f750949e98e2b99a
mariobot <mariobot113@hotmail.com>
2023-06-22 19:01:34 -0500

    UserRequest Model

---
+++ varify-email/Model/UserRegisterRequest.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VarifyEmail.Model
+{
+    public class UserRegisterRequest
+    {
+        [Required, EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [Required, MinLength(6)]
+        public string Password { get; set; } = string.Empty;
+
+        [Required, Compare("Password")]
+        public string ConfirmPassword { get; set; } = string.Empty;
+    }
+}
