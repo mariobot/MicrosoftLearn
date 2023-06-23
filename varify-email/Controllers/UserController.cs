8273956dc6100f9ce6307fcb79f3e90a37397ee9
mariobot <mariobot113@hotmail.com>
2023-06-22 19:13:09 -0500

    CreatePasswodHash method

---
+++ varify-email/Controllers/UserController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
+using VarifyEmail.Data;
+
+namespace VarifyEmail.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly DataContext dataContext;
+
+        public UserController(DataContext dataContext)
+        {
+            this.dataContext = dataContext;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult> Register(UserRegisterRequest userRegisterRequest)
+        { 
+
+            if (this.dataContext.Users.Any(x => x.Email == userRegisterRequest.Email)) 
+            {
+                return BadRequest("Use already exists.");            
+            }
+
+            CreatePasswordHash(userRegisterRequest.Password, out byte[] passwordHash, out byte[] passwordSalt);
+        
+        }
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }        
+        }
+    }
+}
