5067f42369382f6d44f21644cff94ac101ec46e3
mariobot <mariobot113@hotmail.com>
2023-06-26 18:35:43 -0500

    Add UserLoginRequest Model

---
+++ varify-email/Model/UserLoginRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VarifyEmail.Model
+{
+    public class UserLoginRequest
+    {
+        [Required, EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [Required, MinLength(6)]
+        public string Password { get; set; } = string.Empty;
+    }
+}
