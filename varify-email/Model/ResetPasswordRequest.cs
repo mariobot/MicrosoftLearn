9db0aa87dbf39afa44f4bb0fc7c4278189112fbd
mariobot <mariobot113@hotmail.com>
2023-06-27 18:33:21 -0500

    Add resetPasswordRequest Model.

--- varify-email/Model/ResetPasswordRequest.cs
+++ varify-email/Model/ResetPasswordRequest.cs
@@ -1,6 +1,16 @@
-﻿namespace VarifyEmail.Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VarifyEmail.Model
 {
     public class ResetPasswordRequest
     {
+        [Required]
+        public string Token { get; set; } = string.Empty;
+
+        [Required, MinLength(6)]
+        public string Password { get; set; } = string.Empty;
+
+        [Required, Compare("Password")]
+        public string ConfirmPassword { get; set; } = string.Empty;
     }
 }
