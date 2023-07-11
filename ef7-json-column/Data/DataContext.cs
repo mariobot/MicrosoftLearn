33d51b96559c8f63989bf6d910e01ed3397b980c
mariobot <mariobot113@hotmail.com>
2023-07-11 11:31:48 -0500

    Create DbContext

---
+++ ef7-json-column/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Ef7JsonColumns.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+                
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("\"Data Source = MBOTERO\\\\MBOTERO;Initial Catalog = securemail;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False;\"")
+        }
+
+
+    }
+}
