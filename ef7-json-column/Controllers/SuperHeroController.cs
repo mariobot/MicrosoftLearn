db706ddd634d2c35b34ffa94c5ff191ffef6ae0c
mariobot <mariobot113@hotmail.com>
2023-07-11 12:00:31 -0500

    Add SuperHero Controller

---
+++ ef7-json-column/Controllers/SuperHeroController.cs
@@ -0,0 +1,39 @@
+﻿using Ef7JsonColumns.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ef7JsonColumns.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        private readonly DataContext dataContext;
+
+        public SuperHeroController(DataContext dataContext)
+        {
+            this.dataContext = dataContext;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<SuperHero>>> AddHeroes(List<SuperHero> heroes)
+        { 
+
+            this.dataContext.AddRange(heroes);
+
+            await this.dataContext.SaveChangesAsync();
+
+            return Ok(await this.dataContext.Heroes.ToListAsync());        
+        }
+
+        [HttpGet("{city}")]
+        public async Task<ActionResult<List<SuperHero>>> GetHeroes(string city)
+        {
+
+            var heroes = await this.dataContext.Heroes.Where(x => x.Details.City.Contains(city)).ToListAsync();            
+
+            return Ok(heroes);
+        }
+    }
+}
