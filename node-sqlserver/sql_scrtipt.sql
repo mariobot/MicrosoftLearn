71c49f9c62fc15d92e4b9117353c3264842713da
mariobot <mariobot113@hotmail.com>
2023-08-02 19:09:01 -0500

    Add gitignore and sql creation table script

---
+++ node-sqlserver/sql_scrtipt.sql
@@ -0,0 +1,19 @@
+USE [ProductsList]
+GO
+/****** Object:  Table [dbo].[Orders]    Script Date: 8/2/2023 11:44:00 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Orders](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Title] [nvarchar](max) NOT NULL,
+	[Quantity] [int] NOT NULL,
+	[Message] [nvarchar](max) NOT NULL,
+	[City] [nvarchar](max) NOT NULL,
+ CONSTRAINT [PK_Orders] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
