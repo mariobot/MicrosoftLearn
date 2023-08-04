c6617d9de601a4b424a8c235ea6e132172b4e540
mariobot <mariobot113@hotmail.com>
2023-08-03 19:06:14 -0500

    Add Insert data to sql_script

--- node-sqlserver/sql_scrtipt.sql
+++ node-sqlserver/sql_scrtipt.sql
@@ -17,3 +17,28 @@ CREATE TABLE [dbo].[Orders](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
+
+USE [ProductsList]
+GO
+SET IDENTITY_INSERT [dbo].[Orders] ON 
+GO
+INSERT [dbo].[Orders] ([Id], [Title], [Quantity], [Message], [City]) VALUES (1, N'Xbox', 12, N'Microsoft XBox', N'New York')
+GO
+INSERT [dbo].[Orders] ([Id], [Title], [Quantity], [Message], [City]) VALUES (2, N'PlayStation', 10, N'Sony Play Station', N'LA')
+GO
+INSERT [dbo].[Orders] ([Id], [Title], [Quantity], [Message], [City]) VALUES (3, N'Call of Duty', 5, N'Xbox Game', N'New York')
+GO
+INSERT [dbo].[Orders] ([Id], [Title], [Quantity], [Message], [City]) VALUES (4, N'San Andreas', 3, N'Game', N'Washington')
+GO
+INSERT [dbo].[Orders] ([Id], [Title], [Quantity], [Message], [City]) VALUES (5, N'Mario Bros', 4, N'Game', N'LA')
+GO
+INSERT [dbo].[Orders] ([Id], [Title], [Quantity], [Message], [City]) VALUES (6, N'Camera Sony', 2, N'Sony Company', N'New York')
+GO
+INSERT [dbo].[Orders] ([Id], [Title], [Quantity], [Message], [City]) VALUES (8, N'Camera Nikon', 2, N'Black', N'New York')
+GO
+INSERT [dbo].[Orders] ([Id], [Title], [Quantity], [Message], [City]) VALUES (9, N'Plasma monitor', 2, N'Black', N'New York')
+GO
+INSERT [dbo].[Orders] ([Id], [Title], [Quantity], [Message], [City]) VALUES (10, N'Plasma TV', 6, N'Sony Brand', N'New York')
+GO
+SET IDENTITY_INSERT [dbo].[Orders] OFF
+GO
