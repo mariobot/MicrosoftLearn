ef01b66a0c235ad681a89b2e056979ddd41f068b
mariobot <mariobot113@hotmail.com>
2023-08-15 11:57:40 -0500

    Copy clase-3 to clase-4

---
+++ node-js/clase-4/movie.js
@@ -0,0 +1,30 @@
+const z = require('zod')
+
+const movieSchema = z.object({
+    title: z.string(),
+    year: z.number().int().positive().min(1900).max(2024),
+    director: z.string(),
+    duration: z.number().int().positive(),
+    rate: z.number().min(0).max(10).default(0),
+    poster: z.string().url(),
+    genre: z.array(
+        z.enum(['Action', 'Adventure', 'Crime','Comedy', 'Drama','Romance','Fantasy','Horror','Thriller','Sci-Fi']),
+            { 
+                required_error : 'Movie genre is required',
+                invalid_type_error : 'Movie genre must be an array of enum Genre'
+            }
+        )
+})
+
+function ValidateMovie(object){
+    return movieSchema.safeParse(object)
+}
+
+function ValidatePartialMovie(object){
+    return movieSchema.partial().safeParse(object)
+}
+
+module.exports = {
+    ValidateMovie,
+    ValidatePartialMovie
+}
