7edca30c92dd5553020cf51f16f5f701aa714162
mariobot <mariobot113@hotmail.com>
2023-09-13 10:14:24 -0500

    Initialize project VideoGameLibrary

---
+++ blazor-net7/Shared/VideoGame.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleVideoGameLibrary.Shared
+{
+    public class VideoGame
+    {
+        public int Id { get; set; }
+        public required string Title { get; set; }
+        public string Publisher { get; set; } = string.Empty;
+        public int ReleaseYear { get; set; }
+    }
+}
