3e881cdea185920f3c8c3bfdf8bab80bafb4e130
mariobot <mariobot113@hotmail.com>
2023-09-13 17:09:36 -0500

    Add VideoGames component

---
+++ blazor-net7/Server/Controllers/VideoGameController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SimpleVideoGameLibrary.Shared;
+
+namespace SimpleVideoGameLibrary.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VideoGameController : ControllerBase
+    {   
+        public async Task<ActionResult<List<VideoGame>>> GetAllVideoGames() 
+        { 
+            var list = new List<VideoGame>() {
+                new VideoGame { Id = 1, Title = "Tetris", Publisher = "Nintendo", ReleaseYear = 1989},
+                new VideoGame { Id = 2, Title = "Pong", Publisher = "Atari", ReleaseYear = 1972},
+            };
+
+            return Ok(list);        
+        }
+    }
+}
