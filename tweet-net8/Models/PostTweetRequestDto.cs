d7f98b7b67097132c52dcc7698b577670051574e
mariobot <mariobot113@hotmail.com>
2023-09-26 18:12:09 -0500

    Add tweetinvi lib

---
+++ tweet-net8/Models/PostTweetRequestDto.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace TwitterXScheduler.Models
+{
+    public class PostTweetRequestDto
+    {
+        [JsonProperty("text")]
+        public string Text { get; set; } = string.Empty;
+    }
+}
