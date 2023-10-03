3de6dd8506fd69eabfc8436dc2827d6aeacc4cfb
mariobot <mariobot113@hotmail.com>
2023-10-03 17:10:40 -0500

    Add DataContext to project

--- auth-net8/Data/DataContext.cs
+++ auth-net8/Data/DataContext.cs
@@ -1,8 +1,13 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace DotNet8Authentication.Data
 {
     public class DataContext : IdentityDbContext
     {
+        public DataContext(DbContextOptions<DataContext> options) : base (options) 
+        {   
+                
+        }
     }
 }
