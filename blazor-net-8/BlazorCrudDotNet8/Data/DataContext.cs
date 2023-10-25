03365fe1f57bf691d83585780b09ee516b294535
mariobot <mariobot113@hotmail.com>
2023-10-25 16:00:57 -0500

    Add DataContext class

---
+++ blazor-net-8/BlazorCrudDotNet8/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using BlazorCrudDotNet8.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorCrudDotNet8.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+                
+        }
+
+        public DbSet<Game> Games { get; set; }
+    }
+}
