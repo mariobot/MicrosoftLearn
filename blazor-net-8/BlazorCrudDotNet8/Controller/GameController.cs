71e4ab653aaa35683b7bd52ea3e6feff52efca56
mariobot <mariobot113@hotmail.com>
2023-10-30 17:54:33 -0500

    Rename Shared Project.

---
+++ blazor-net-8/BlazorCrudDotNet8/Controller/GameController.cs
@@ -0,0 +1,25 @@
+﻿using BlazorCrudDotNet8.Shared.Entity;
+using BlazorCrudDotNet8.Shared.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorCrudDotNet8.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GameController : ControllerBase
+    {
+        private readonly IGameService gameService;
+
+        public GameController(IGameService gameService)
+        {
+            this.gameService = gameService;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Game>> AddGame(Game game)
+        { 
+            var addGame = await this.gameService.AddGame(game);
+            return Ok(addGame);
+        }
+    }
+}
