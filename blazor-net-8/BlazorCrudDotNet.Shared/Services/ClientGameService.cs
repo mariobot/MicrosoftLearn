2a8c7ed1a92324026d683028d5e8c994a0ceb441
mariobot <mariobot113@hotmail.com>
2023-10-30 18:22:20 -0500

    Implement ClientGameService

---
+++ blazor-net-8/BlazorCrudDotNet.Shared/Services/ClientGameService.cs
@@ -0,0 +1,35 @@
+﻿using BlazorCrudDotNet8.Shared.Data;
+using BlazorCrudDotNet8.Shared.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorCrudDotNet8.Shared.Services
+{
+    public class ClientGameService : IGameService
+    {
+        private readonly HttpClient httpClient;
+
+        public ClientGameService(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+
+        public async Task<List<Game>> GetAllGames()
+        {
+
+            return null;
+            //var result = await this.httpClient.GetAsync("/games");
+            //return await result.Content.ReadFromJsonAsAsyncEnumerable<Game>();
+        }
+
+        public async Task<Game> AddGame(Game game)
+        {
+            var result = await this.httpClient.PostAsJsonAsync<Game>("/api/game", game);
+            return await result.Content.ReadFromJsonAsync<Game>();
+        }
+    }
+}
