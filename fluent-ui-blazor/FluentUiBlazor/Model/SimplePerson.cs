dcba2e9ea00c416dbeacbddc6ad698281f462b6d
mariobot <mariobot113@hotmail.com>
2023-11-13 18:02:01 -0500

    Add SimplePerson Model

---
+++ fluent-ui-blazor/FluentUiBlazor/Model/SimplePerson.cs
@@ -0,0 +1,9 @@
+﻿namespace FluentUiBlazor.Model
+{
+    public class SimplePerson
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public int Age { get; set; }
+    }
+}
