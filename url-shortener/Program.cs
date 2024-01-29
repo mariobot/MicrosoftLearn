6dca061de284b26c4b065c1ad0799e497a41f1f0
mariobot <mariobot113@hotmail.com>
2024-01-29 18:56:23 -0500

    Add UrlShortener Project

---
+++ url-shortener/Program.cs
@@ -0,0 +1,30 @@
+using Microsoft.EntityFrameworkCore;
+using UrlShortener.Context;
+using UrlShortener.Endpoints;
+using UrlShortener.Service;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
+
+builder.Services.AddTransient<UrlShorteningService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapShortenerUrlEndpoints();
+
+app.Run();
