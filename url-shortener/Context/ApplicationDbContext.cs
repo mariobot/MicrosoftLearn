62567e7c54dfa10fd3dbbb0d74e3ae430172531f
mariobot <mariobot113@hotmail.com>
2024-01-29 18:56:44 -0500

    Add App Context and Models

---
+++ url-shortener/Context/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UrlShortener.Model;
+
+namespace UrlShortener.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options): base(options) 
+        {                
+        }
+
+        public DbSet<ShortenerUrl> ShortenedUrls { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ShortenerUrl>(builder =>
+            {
+                builder
+                    .Property(shortenerUrl => shortenerUrl.Code)
+                    .HasMaxLength(ShortLinkSettings.Length);
+
+                builder
+                    .HasIndex(shortenerUrl => shortenerUrl.Code)
+                    .IsUnique();
+            });
+        }
+    }
+}
---
+++ url-shortener/Model/ShortLinkSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace UrlShortener.Model
+{
+    public static class ShortLinkSettings
+    {
+        public const int Length = 7;
+        public const string Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    }
+}
