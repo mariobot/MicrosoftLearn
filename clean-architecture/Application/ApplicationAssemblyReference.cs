74a391f11db749af63226e2827f45fcc668206f3
mariobot <mariobot113@hotmail.com>
2024-03-06 18:47:42 -0500

    Add Assembly Reference

---
+++ clean-architecture/Application/ApplicationAssemblyReference.cs
@@ -0,0 +1,9 @@
+using System.Reflection;
+
+namespace Application;
+
+public class ApplicationAssemblyReference
+{
+    internal static readonly Assembly Assembly = typeof(ApplicationAssemblyReference).Assembly;
+}
+
