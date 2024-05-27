0008426394fab58aa43c6f1c8bad8348e79e6366
mariobot <mariobot113@hotmail.com>
2024-05-27 18:57:10 -0500

    Add Extensions

---
+++ fluent-ui-blazor/FluentUiBlazor/Components/Extensions/EnumExtensions.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel;
+using System.Reflection;
+
+namespace FluentUiBlazor.Components.Extensions
+{
+    public static class EnumExtensions
+    {
+        public static string? ToAttributeValue<TEnum>(this TEnum? value, bool lowercase = true) where TEnum : struct, Enum
+        => value == null ? null : ToAttributeValue(value.Value, lowercase);
+
+        public static string? ToAttributeValue<TEnum>(this TEnum value, bool lowercase = true) where TEnum : struct, Enum
+            => GetDescription(value, lowercase);
+
+        public static string? GetDescription<TEnum>(this TEnum value, bool lowercase = true) where TEnum : struct, IConvertible
+        {
+            if (!typeof(TEnum).IsEnum)
+            {
+                return null;
+            }
+
+            var description = value.ToString();
+
+            FieldInfo? fieldInfo = value.GetType().GetField(value.ToString() ?? "");
+            if (fieldInfo != null)
+            {
+                var attributes = fieldInfo.GetCustomAttributes(typeof(DescriptionAttribute), true);
+                if (attributes?.Length > 0)
+                {
+                    description = ((DescriptionAttribute)attributes[0]).Description;
+                }
+            }
+
+            if (lowercase)
+            {
+                return description?.ToLowerInvariant();
+            }
+
+            return description;
+        }
+    }
+}
