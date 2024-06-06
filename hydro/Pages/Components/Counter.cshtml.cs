1a6bbd91abfe8e637cc428040f823f51c4461505
mariobot <mariobot113@hotmail.com>
2024-06-06 16:53:50 -0500

    Add Counter component

---
+++ hydro/Pages/Components/Counter.cshtml.cs
@@ -0,0 +1,11 @@
+﻿namespace HydroProject.Pages.Components;
+
+public class Counter : HydroComponent
+{
+    public int Count { get; set; }
+
+    public void Add()
+    { 
+        Count++;
+    }
+}
