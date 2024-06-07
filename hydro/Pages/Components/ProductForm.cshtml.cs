ad8cb76c19216cef3c8b6c06cc7f125878e13ca0
mariobot <mariobot113@hotmail.com>
2024-06-07 17:12:17 -0500

    Form validation component

---
+++ hydro/Pages/Components/ProductForm.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HydroProject.Pages.Components;
+
+public class ProductForm : HydroComponent
+{
+    [Required, MaxLength(50)]
+    public string Name { get; set; }
+
+    [Required, MaxLength(50)]
+    public string Location { get; set; }
+
+    public string Result { get; set; }
+
+    public async Task Submit()
+    {
+        if (!Validate())
+        {
+            Result = string.Empty;
+            return;
+        }
+
+        Result = $"Your Product {Name} will dispatch at {Location}";
+    }
+}
