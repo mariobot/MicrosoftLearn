87bce2e776736757f4856d0abca80d1259df3379
mariobot <mariobot113@hotmail.com>
2024-06-07 16:03:29 -0500

    Navigation links components

---
+++ hydro/Pages/Components/Links.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HydroProject.Pages.Components;
+
+public class Links: HydroComponent
+{
+    public void CallPrivancyLink()
+    {
+        Location(Url.Page("/Privacy"));
+    }
+}
