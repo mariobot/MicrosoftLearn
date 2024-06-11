1e619d06595787c744b806f0f94adb948d3a3b1e
mariobot <mariobot113@hotmail.com>
2024-06-11 17:56:14 -0500

    Long Polling component

---
+++ hydro/Pages/Components/LongPolling.cshtml.cs
@@ -0,0 +1,13 @@
+﻿namespace HydroProject.Pages.Components;
+
+public class LongPolling : HydroComponent
+{
+    public int RandomNumber { get; set; }
+
+    [Poll(Interval = 1_000)]
+    public async Task Refresh()
+    { 
+        Random random = new Random();
+        RandomNumber = random.Next(0, int.MaxValue);
+    }
+}
