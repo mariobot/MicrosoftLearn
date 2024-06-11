638db18472fefbc87e2f3ee22977fa4cb581b0b3
mariobot <mariobot113@hotmail.com>
2024-06-11 17:56:34 -0500

    Toast component

---
+++ hydro/Pages/Components/Toasts.cshtml.cs
@@ -0,0 +1,31 @@
+﻿namespace HydroProject.Pages.Components;
+
+public class Toasts: HydroComponent
+{
+    public List<Toast> ToastsList { get; set; } = new();
+
+    public Toasts()
+    {
+        Subscribe<UnhandledHydroError>(Handle);
+    }
+
+    private void Handle(UnhandledHydroError data) =>
+        ToastsList.Add(new Toast(
+            Id: Guid.NewGuid().ToString("N"),
+            Message: data.Message,
+            Type: ToastType.Error
+        ));
+
+    public void Close(string id) =>
+        ToastsList.RemoveAll(t => t.Id == id);
+
+    public record Toast(string Id, string Message, ToastType Type);
+
+    public enum ToastType
+    { 
+        Error,
+        Success,
+        Log,
+        Information
+    }
+}
