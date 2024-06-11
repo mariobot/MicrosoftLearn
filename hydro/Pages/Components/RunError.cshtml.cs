e5e115abf0dab13ae0fd24fc1a7fa73cce59553e
mariobot <mariobot113@hotmail.com>
2024-06-11 17:56:27 -0500

    Run Error component

---
+++ hydro/Pages/Components/RunError.cshtml.cs
@@ -0,0 +1,9 @@
+﻿namespace HydroProject.Pages.Components;
+
+public class RunError : HydroComponent
+{
+    public void ExecuteRun()
+    { 
+        throw new Exception("This error message is for testing mode");
+    }
+}
--- hydro/Program.cs
+++ hydro/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.AspNetCore.Diagnostics;
+using System.Net;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -10,10 +13,33 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.UseExceptionHandler(b => b.Run(async context =>
+{
+    if (!context.IsHydro())
+    {
+        return;
+    }
+
+    var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+    switch (contextFeature?.Error)
+    {
+        // custom cases for custom exception types if needed
+
+        default:
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            await context.Response.WriteAsJsonAsync(new UnhandledHydroError(
+                Message: "There was a problem with this operation and it wasn't finished",
+                Data: null
+            ));
+
+            return;
+    }
+}));
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
