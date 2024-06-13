431b1e006422c4c7c326fe022503cdcf5c6590c8
mariobot <mariobot113@hotmail.com>
2024-06-13 18:40:35 -0500

    Add Loading component

---
+++ hydro/Pages/Components/Loading.cshtml.cs
@@ -0,0 +1,11 @@
+﻿namespace HydroProject.Pages.Components;
+
+public class Loading : HydroComponent
+{
+    public int ValueRange { get; set; } = 1;
+    
+    public async Task Submit()
+    {
+        await Task.Delay(ValueRange*1000);
+    }
+}
