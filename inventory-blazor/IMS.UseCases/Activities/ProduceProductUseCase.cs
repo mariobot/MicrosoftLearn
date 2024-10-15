ef672eb6859da8581d9e2c21ae1130e36b31fbc3
mariobot <mariobot113@hotmail.com>
2024-10-15 15:57:41 -0500

    Add Activities

---
+++ inventory-blazor/IMS.UseCases/Activities/ProduceProductUseCase.cs
@@ -0,0 +1,34 @@
+﻿using IMS.CoreBusiness;
+using IMS.UseCases.PluginInterfaces;
+
+namespace IMS.UseCases.Activities
+{
+    public class ProduceProductUseCase : IProduceProductUseCase
+    {
+        private readonly IInventoryRepository inventoryRepository;
+        private readonly IProductRepository productRepository;
+        private readonly IInventoryTransactionsRepository inventoryTransactionRepository;
+        private readonly IProductTransactionRepository productTransactionRepository;
+
+        public ProduceProductUseCase(
+            IInventoryRepository inventoryRepository,
+            IProductRepository productRepository,
+            IInventoryTransactionsRepository inventoryTransactionRepository,
+            IProductTransactionRepository productTransactionRepository)
+        {
+            this.inventoryRepository = inventoryRepository;
+            this.productRepository = productRepository;
+            this.inventoryTransactionRepository = inventoryTransactionRepository;
+            this.productTransactionRepository = productTransactionRepository;
+        }
+
+        public async Task ExecuteAsync(string productionNumber, Product product, int quantity, string doneBy)
+        {
+            await this.productTransactionRepository.ProduceAsync(productionNumber, product, quantity, product.Price, doneBy);
+
+            product.Quantity += quantity;
+
+            await this.productRepository.UpdateProductAsync(product);
+        }
+    }
+}
---
+++ inventory-blazor/IMS.UseCases/Activities/PurchaseInventoryUseCase .cs
@@ -0,0 +1,28 @@
+﻿using IMS.CoreBusiness;
+using IMS.UseCases.PluginInterfaces;
+
+namespace IMS.UseCases.Activities
+{
+    public class PurchaseInventoryUseCase: IPurchaseInventoryUseCase
+    {
+        private readonly IInventoryTransactionsRepository inventoryTransactionsRepository;
+        private readonly IInventoryRepository InventoryRepository;
+
+        public PurchaseInventoryUseCase(
+            IInventoryTransactionsRepository inventoryTransactionRepository,
+            IInventoryRepository inventoryRepository)
+        {
+
+            this.inventoryTransactionsRepository = inventoryTransactionRepository;
+            this.InventoryRepository = inventoryRepository;
+        }
+
+        public async Task ExecuteAsync(string poNumber, Inventory inventory, int quantity, string doneBy)
+        {
+            await this.inventoryTransactionsRepository.PurchaseAsync(poNumber, inventory, quantity, inventory.Price, doneBy);
+
+            inventory.Quantity += quantity;
+            await this.InventoryRepository.UpdateInventoryAsync(inventory);
+        }
+    }
+}
