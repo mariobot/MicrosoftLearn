43b9d6a549396c6d8303fb03c288d8b858f5223e
mariobot <mariobot113@hotmail.com>
2025-05-08 18:46:58 -0500

    Add the login mechanims

---
+++ linked-in-learn/MyApi/Controllers/TestController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class TestController : ControllerBase
+{
+    [HttpGet("public")]
+    public IActionResult Public()
+    {
+        return Ok(nameof(Public));
+    }
+
+    [Authorize]
+    [HttpGet("protected")]
+    public IActionResult Protected()
+    {
+        return Ok(nameof(Protected));
+    }
+}
