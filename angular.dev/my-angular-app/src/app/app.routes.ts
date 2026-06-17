8d7dd3be417c8b1b257e4c5ec2c9d27821051303
mariobot <mariobot113@hotmail.com>
2026-06-16 19:45:29 -0500

    Add new components to route

--- angular.dev/my-angular-app/src/app/app.routes.ts
+++ angular.dev/my-angular-app/src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { About } from './about/about';
 import { Contact } from './contact/contact';
+import { Interpolation } from './Templates/interpolation';
+import { Reference } from './Templates/reference';
 
 export const routes: Routes = [
   { path: 'about', component: About },
   { path: 'contact', component: Contact },
+  { path: 'interpolation', component: Interpolation },
+  { path: 'reference', component: Reference },
 ];
