58a5979570bd1e9b18efba5f315cd2b7afefd59b
mariobot <mariobot113@hotmail.com>
2026-06-17 20:30:52 -0500

    Fix some details abaut the project

--- angular.dev/my-angular-app/src/app/component/services2Comp.ts
+++ angular.dev/my-angular-app/src/app/component/services2Comp.ts
@@ -6,22 +6,47 @@ export class CounterService {
   value = 0;
   inc() { this.value++; }
   dec() { this.value--; }
-  reset() { this.value = 0; }
+}
+
+@Component({
+  selector: 'counter-a',
+  standalone: true,
+  template: `
+    <h4>Counter A</h4>
+    <p>Value: {{ counter.value }}</p>
+    <button (click)="counter.inc()">+1</button>
+    <button (click)="counter.dec()">-1</button>
+  `
+})
+export class CounterA {
+  constructor(public counter: CounterService) {}
+}
+
+@Component({
+  selector: 'counter-b',
+  standalone: true,
+  template: `
+    <h4>Counter B</h4>
+    <p>Value: {{ counter.value }}</p>
+    <button (click)="counter.inc()">+1</button>
+    <button (click)="counter.dec()">-1</button>
+  `
+})
+export class CounterB {
+  constructor(public counter: CounterService) {}
 }
 
 @Component({
   selector: 'app-root',
   standalone: true,
+  imports: [CounterA, CounterB],
   template: `
-    <h3>Services</h3>
-    <p>Counter: {{ counter.value }}</p>
-    <button (click)="counter.inc()">+</button>
-    <button (click)="counter.dec()">-</button>
-    <button (click)="counter.reset()">Reset</button>
+    <h3>Shared Service Across Components</h3>
+    <counter-a></counter-a>
+    <counter-b></counter-b>
+    <p><em>Both components use the same CounterService instance.</em></p>
   `
 })
-export class Services2Comp {
-  constructor(public counter: CounterService) {}
-}
+export class Services2Comp {}
 
 bootstrapApplication(Services2Comp);
--- angular.dev/my-angular-app/src/app/contact/contact.ts
+++ angular.dev/my-angular-app/src/app/contact/contact.ts
@@ -5,7 +5,10 @@ import { Component } from '@angular/core';
   standalone: true,
   template: `
     <h2>Contact</h2>
-    <p>This is the contact page for my Angular app.</p>
+    <p>This application is a lab tutorial from W3School from the Angular topic.</p>
+    <p>Autor: W3School, developed: Mario Botero </p>
+    <p>Content at <a href="https://www.w3schools.com/angular/">W3School Angular</a></p>
+    <p>Source code at <a href="https://github.com/mariobot/MicrosoftLearn/tree/master/angular.dev/my-angular-app">https://github.com/mariobot/MicrosoftLearn/tree/master/angular.dev/my-angular-app</a>
   `,
   styles: [`
     :host {
