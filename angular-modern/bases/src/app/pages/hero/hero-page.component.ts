4e435c4cb62851c4678a8c86c4c6ab41f917003e
mariobot <mariobot113@hotmail.com>
2026-06-26 16:48:55 -0500

    Hero page component task.

--- angular-modern/bases/src/app/pages/hero/hero-page.component.ts
+++ angular-modern/bases/src/app/pages/hero/hero-page.component.ts
@@ -1,13 +1,21 @@
-import { Component, signal } from "@angular/core";
+import { Component, computed, signal } from "@angular/core";
+import { UpperCasePipe } from "@angular/common";
 
 @Component({
     templateUrl: './hero-page.component.html',
+    imports: [UpperCasePipe]
 })
 
 export class HeroPageComponent {
     
     name = signal('Ironman');
     age = signal(45);
+    heroDescription = computed(() =>{
+        const description = `${this.name()} - ${this.age()}`;
+        return description;
+    })
+
+    capitalizedName = computed(() => this.name().toUpperCase());
 
     getHeroDescription() {
         return `${this.name()} - ${this.age()}`;
