19b40dccbf2d448c54e12da3e49f46fa4a000d5e
mariobot <mariobot113@hotmail.com>
2026-06-26 18:13:29 -0500

    Add if component to the list of characters.

--- angular-modern/bases/src/app/pages/dragonball/dragon-page.component.ts
+++ angular-modern/bases/src/app/pages/dragonball/dragon-page.component.ts
@@ -16,5 +16,6 @@ export class DragonPageComponent {
         { id: 1, name: 'Goku', power: 15000 },
         { id: 2, name: 'Vegeta', power: 7500 },
         { id: 3, name: 'Trunks', power: 5000 },
+        { id: 4, name: 'Yamcha', power: 500 },
     ])
 }
