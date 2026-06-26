77d025cc2b14ab9d2ce5913714cc07825d7dc45f
mariobot <mariobot113@hotmail.com>
2026-06-26 16:12:47 -0500

    Signals in counter

--- angular-modern/bases/src/app/pages/counter/counter.component.ts
+++ angular-modern/bases/src/app/pages/counter/counter.component.ts
@@ -1,8 +1,9 @@
-import { Component } from "@angular/core";
+import { Component, signal } from "@angular/core";
 
 @Component({
     template: `
         <h1>Counter {{ counter }}</h1>
+        <h2>Counter Signal {{ counterSignal() }}</h2>
         <p>counter component</p>
         <button (click)="increment(1)" >+1</button>
         <button (click)="increment(-1)" >-1</button>
@@ -12,10 +13,14 @@ import { Component } from "@angular/core";
 
 export class CounterPageComponent {
     counter = 10;
+    counterSignal = signal(10);
+
     increment(value: number) {
         this.counter += value;
+        this.counterSignal.update((current) => current + value);
     }
     reset() {
         this.counter = 10;
+        this.counterSignal.set(10)
     }
 }
