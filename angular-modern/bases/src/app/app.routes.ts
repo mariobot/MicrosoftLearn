fd97743358a46588877e51d91bad1f66735c9540
mariobot <mariobot113@hotmail.com>
2026-06-26 17:15:46 -0500

    Add navbar and routerlinks to navegate.

--- angular-modern/bases/src/app/app.routes.ts
+++ angular-modern/bases/src/app/app.routes.ts
@@ -10,5 +10,9 @@ export const routes: Routes = [
     {
         path: 'hero',
         component: HeroPageComponent,
+    },
+    {
+        path: '**',
+        redirectTo: '',
     }
 ];
---
+++ angular-modern/bases/src/app/components/shared/navbar/navbar.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
+
+@Component({
+  selector: 'app-navbar',
+  imports: [RouterLink],
+  templateUrl: './navbar.html',
+})
+export class Navbar {}
