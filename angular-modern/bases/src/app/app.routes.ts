3e99868d05fc4e008fdc321fac7cb7df7bb2d968
mariobot <mariobot113@hotmail.com>
2026-06-26 17:52:11 -0500

    Add dragon page component.

--- angular-modern/bases/src/app/app.routes.ts
+++ angular-modern/bases/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { CounterPageComponent } from './pages/counter/counter.component';
 import { HeroPageComponent } from './pages/hero/hero-page.component';
+import { DragonPageComponent } from './pages/dragonball/dragon-page.component';
 
 export const routes: Routes = [
     {
@@ -12,7 +13,11 @@ export const routes: Routes = [
         component: HeroPageComponent,
     },
     {
+        path: 'dragonball',
+        component: DragonPageComponent,
+    },
+    {
         path: '**',
         redirectTo: '',
-    }
+    },
 ];
---
+++ angular-modern/bases/src/app/pages/dragonball/dragon-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+    templateUrl: './dragon-page.component.html',
+})
+
+export class DragonPageComponent {
+}
