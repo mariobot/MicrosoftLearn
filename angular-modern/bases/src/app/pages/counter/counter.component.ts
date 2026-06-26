e961943631b66c944cf8db47f61ae6016f60571d
mariobot <mariobot113@hotmail.com>
2026-06-26 16:03:26 -0500

    Homework counter

--- angular-modern/bases/src/app/pages/counter/counter.component.ts
+++ angular-modern/bases/src/app/pages/counter/counter.component.ts
@@ -5,12 +5,17 @@ import { Component } from "@angular/core";
         <h1>Counter {{ counter }}</h1>
         <p>counter component</p>
         <button (click)="increment(1)" >+1</button>
+        <button (click)="increment(-1)" >-1</button>
+        <button (click)="reset()" >Reset</button>
     `,
 })
 
 export class CounterPageComponent {
-    counter = 0;
+    counter = 10;
     increment(value: number) {
         this.counter += value;
+    }
+    reset() {
+        this.counter = 10;
     }
 }
