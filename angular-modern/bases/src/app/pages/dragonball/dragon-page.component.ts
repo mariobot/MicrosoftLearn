3293f65a37673fb8836685486c35412444990e6e
mariobot <mariobot113@hotmail.com>
2026-06-26 18:34:05 -0500

    Add components to list powers

--- angular-modern/bases/src/app/pages/dragonball/dragon-page.component.ts
+++ angular-modern/bases/src/app/pages/dragonball/dragon-page.component.ts
@@ -12,10 +12,33 @@ interface Character {
 
 export class DragonPageComponent {
 
+    name = signal('Gohan');
+    power = signal(1000);
+
     characters = signal<Character[]>([
         { id: 1, name: 'Goku', power: 15000 },
         { id: 2, name: 'Vegeta', power: 7500 },
         { id: 3, name: 'Trunks', power: 5000 },
         { id: 4, name: 'Yamcha', power: 500 },
     ])
+
+    addCharacter() {
+        if(!this.name() || !this.power() || this.power() <= 0) return;
+        
+        const newCharacter: Character = {
+            id: this.characters().length + 1,
+            name: this.name(),
+            power: this.power()
+        }
+
+        this.characters.update((list) => [...list, newCharacter])
+        
+        //console.log(this.name() + this.power())
+        this.resetFields()
+    }
+
+    resetFields() {
+        this.name.set('');
+        this.power.set(0);
+    }
 }
