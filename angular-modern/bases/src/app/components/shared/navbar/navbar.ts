0dd2af53960510d90e84bb42ff5fe7c57df44992
mariobot <mariobot113@hotmail.com>
2026-06-26 17:27:53 -0500

    Add navigation styles and routerLink actions and options.

--- angular-modern/bases/src/app/components/shared/navbar/navbar.ts
+++ angular-modern/bases/src/app/components/shared/navbar/navbar.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterLink } from "@angular/router";
+import { RouterLink, RouterLinkActive } from "@angular/router";
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './navbar.html',
 })
 export class Navbar {}
