711029369078ca8929020c9e219c7ab0352910ab
mariobot <mariobot113@hotmail.com>
2026-06-26 21:19:44 -0500

    Initial project gifs app

--- angular-modern/gifs-app/src/app/app.config.ts
+++ angular-modern/gifs-app/src/app/app.config.ts
@@ -2,15 +2,10 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes),
-    {
-      provide: LocationStrategy,
-      useClass: HashLocationStrategy
-    }
+    provideRouter(routes)
   ]
 };
