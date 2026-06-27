a463c4b63fc32108fffa9e5fc3dfd12ece0853e9
mariobot <mariobot113@hotmail.com>
2026-06-26 20:44:57 -0500

    Add save to storage implementation and logic to load and save.

--- angular-modern/bases/src/app/services/dragon.service.ts
+++ angular-modern/bases/src/app/services/dragon.service.ts
@@ -1,15 +1,21 @@
-import { Injectable, signal } from '@angular/core';
+import { effect, Injectable, signal } from '@angular/core';
 import { Character } from '../interfaces/character.interface';
+
+const loadFromLocalStorage = (): Character[] => {
+  const chatacters = localStorage.getItem('characters');
+  return chatacters ? JSON.parse(chatacters) : [];
+}
 
 @Injectable({
   providedIn: 'root',
 })
 export class DragonService {
 
-  characters = signal<Character[]>([
-    { id: 1, name: 'Goku', power: 15000 },
-    { id: 2, name: 'Vegeta', power: 7500 },
-  ])
+  characters = signal<Character[]>(loadFromLocalStorage())
+
+  saveToLocalStorage = effect(() => {
+    localStorage.setItem('characters', JSON.stringify(this.characters()))
+  })
 
   addCharacter(character: Character) {
     this.characters.update((list) => [...list, character])
