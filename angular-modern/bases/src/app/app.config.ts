af788c2c0822e006d5dc567e19d3366e252ca222
mariobot <mariobot113@hotmail.com>
2026-06-26 21:10:29 -0500

    Add Hash strategy

--- angular-modern/bases/src/app/app.config.ts
+++ angular-modern/bases/src/app/app.config.ts
@@ -2,10 +2,15 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    {
+      provide: LocationStrategy,
+      useClass: HashLocationStrategy
+    }
   ]
 };
