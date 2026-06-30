8348af3649d3a9c6379342e59a639b12b0ecb130
mariobot <mariobot113@hotmail.com>
2026-06-30 16:17:58 -0500

    Add options with components per option

--- angular-modern/gifs-app/src/app/gifs/component/side-menu-options/side-menu-options.ts
+++ angular-modern/gifs-app/src/app/gifs/component/side-menu-options/side-menu-options.ts
@@ -1,9 +1,32 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from "@angular/router";
+
+interface MenuOption{
+  label: string;
+  subLabel: string;
+  route: string;
+  icon: string
+}
 
 @Component({
   selector: 'app-side-menu-options',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './side-menu-options.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SideMenuOptions {}
+export class SideMenuOptions {
+  menuOptions: MenuOption[] = [
+    {
+      icon: 'fa-solid fa-chart fa-chart-line',
+      label: 'Trading',
+      subLabel: 'Gifs Populars',
+      route: '/dashboard/trading'
+    },
+    {
+      icon: 'fa-solid fa-magnifyinf-glass',
+      label: 'Search',
+      subLabel: 'Search Gifs',
+      route: '/dashboard/search'
+    }
+  ]
+}
