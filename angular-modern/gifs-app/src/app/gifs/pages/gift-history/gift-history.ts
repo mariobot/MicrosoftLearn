a95b07c26f8a8e9595edb53c376521108941933f
mariobot <mariobot113@hotmail.com>
2026-07-01 09:53:32 -0500

    Add paramter observable and get param.

--- angular-modern/gifs-app/src/app/gifs/pages/gift-history/gift-history.ts
+++ angular-modern/gifs-app/src/app/gifs/pages/gift-history/gift-history.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { map, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-gift-history',
   imports: [],
-  templateUrl: './gift-history.html',  
+  templateUrl: './gift-history.html',
 })
 export default class GiftHistory {
-
+  query = toSignal(
+    inject(ActivatedRoute).params.pipe(
+      map((params) => params['query'])
+    )
+  );
 }
+
+
