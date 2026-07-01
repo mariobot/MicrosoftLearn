a21bb5121402e9df4b9c03f7828307f40a617462
mariobot <mariobot113@hotmail.com>
2026-07-01 08:55:14 -0500

    Add the search functionality

--- angular-modern/gifs-app/src/app/gifs/pages/search-page/search-page.ts
+++ angular-modern/gifs-app/src/app/gifs/pages/search-page/search-page.ts
@@ -1,8 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { GiftList } from "../../component/gift-list/gift-list";
+import { GifService } from '../../services/gifts.service';
+import { Gif } from '../../interfaces/gift.inteface';
 
 @Component({
   selector: 'app-search-page',
-  imports: [],
+  imports: [GiftList],
   templateUrl: './search-page.html',
 })
-export default class SearchPage {}
+export default class SearchPage {
+
+  gifService = inject(GifService);
+  gifs = signal<Gif[]>([])
+
+  onSearch(query: string) {
+    this.gifService.searchGifts(query).subscribe((gifts) => {
+      this.gifs.set(gifts);
+    });
+  }
+}
--- angular-modern/gifs-app/src/app/gifs/services/gifts.service.ts
+++ angular-modern/gifs-app/src/app/gifs/services/gifts.service.ts
@@ -4,6 +4,7 @@ import { environment } from "@environments/environment";
 import type { GiphyResponse } from "../interfaces/giphy.interface";
 import { Gif } from "../interfaces/gift.inteface";
 import { GifMapper } from "../mapper/gift.mapper";
+import { map } from "rxjs";
 
 @Injectable({providedIn:'root'})
 export class GifService{
@@ -28,5 +29,21 @@ export class GifService{
             this.trendingGifsLoading.set(false);
             console.log({gifts});
         });
+    }
+
+    searchGifts(query: string){
+        return this.http.get<GiphyResponse>(`${ environment.giphyUrl}/gifs/search`, {
+            params: {
+                api_key: environment.giphyApiKey,
+                limit: 20,
+                q: query
+            }        
+        })
+        .pipe(
+            map((resp) => {
+                const gifts = GifMapper.mapGiphyItemToGifArray(resp.data)
+                return gifts;
+            })
+        );
     }
 }
