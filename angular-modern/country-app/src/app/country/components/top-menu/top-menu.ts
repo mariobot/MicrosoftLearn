5c29a1d23c79fb01cbcd96e6d5c35a793dd80150
mariobot <mariobot113@hotmail.com>
2026-07-01 19:10:02 -0500

    Add top menu component and navs

---
+++ angular-modern/country-app/src/app/country/components/top-menu/top-menu.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-top-menu',
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './top-menu.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class TopMenu {}
--- angular-modern/country-app/src/app/country/layouts/country-layout/country-layout.ts
+++ angular-modern/country-app/src/app/country/layouts/country-layout/country-layout.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from "@angular/router";
+import { TopMenu } from "../../components/top-menu/top-menu";
 
 @Component({
   selector: 'app-country-layout',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TopMenu],
   templateUrl: './country-layout.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
